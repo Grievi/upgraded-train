17b71fbf3599e7fde18d34d5672d62fe862cfe2d
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-19 23:35:07 +0300

    rename class role to class pitch

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from . import db
 
-class Role(db.Model):
-    __tablename__ = 'roles'
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
 
     id=db.Column(db.Integer,primary_key = True)
     name=db.Column(db.Strimg(255))
+    users=db.relationship('User',backref = 'pitch', lazy="dynamic")
 
     def __repr__(self):
         return f'User {self.name}'
@@ -13,6 +14,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer, db.Foreignkey('pitches.id'))
 
     def __repr__(self):
         return f'User {self.name}'
